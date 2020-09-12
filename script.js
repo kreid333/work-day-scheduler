f0ab03d7cea56960e9f664f22258b6b686cc15ca
kreid333 <kai.j.reid@gmail.com>
2020-09-12 16:43:28 -0400

    made textbox colors dynamic by comparing each textbox number to the current hour

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ $(document).ready(function () {
   var currentDate = $("#currentDay");
   currentDate.text(moment().format("MMMM Do, YYYY"));
 
+  // Establishing the current hour; parseInt to make it a number
+  var currentHour = parseInt(moment().format("HH"));
+
+  // Using the .each iterator function to determine if the name's given to each textarea match the current hour or not
+  $("textarea").each(function () {
+    var nameNumber = parseInt($(this).attr("name"));
+    if (nameNumber < currentHour) {
+      $(this).addClass("past");
+    } else if (nameNumber > currentHour) {
+      $(this).addClass("future");
+    } else if (nameNumber === currentHour) {
+      $(this).addClass("present");
+    }
+  });
+
 });
