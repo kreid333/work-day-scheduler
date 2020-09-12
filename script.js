cb302825ba03301b35a8cc65c9a5544a4b9e787a
kreid333 <kai.j.reid@gmail.com>
2020-09-12 13:42:35 -0400

    added current date to the top of the page

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+$(document).ready(function () {
+  
+  // Establishing current date located at the top of the page 
+  var currentDate = $("#currentDay");
+  currentDate.text(moment().format("MMMM Do, YYYY"));
 
+});
